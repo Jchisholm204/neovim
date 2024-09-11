bc362df0a81241f3ca0bff0a5e2631d81c2f16d1
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-09-11 00:39:19 -0400

    colors are off

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,12 @@
 return {
-    "catppuccin/nvim",
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     lazy = false,
     config = function()
+        -- require('catppuccin').setup({
+        --     -- transparent_background = true,
+        -- })
         vim.cmd.colorscheme("catppuccin")
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
