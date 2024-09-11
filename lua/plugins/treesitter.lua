45523a8a57f85482c0de48fd954d1e297b66e401
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-09-11 01:06:16 -0400

    ts broken

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,18 +5,21 @@ return {
   lazy = false, 
   priority = 1000,
   cmd = {"TSUpdate", "TSInstall"},
-  opts_extend = { "ensure_installed" },
-  opts = {
-    -- highlight = { enable = true },
-    -- indent = { enable = true },
-    ensure_installed = {
-      "bash",
-      "c",
-      "lua",
-    },
-  },
-  ---@param opts TSConfig
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+  config = function()
+      require'nvim-treesitter.configs'.setup {
+          -- A list of parser names, or "all" (the five listed parsers should always be installed)
+          ensure_installed = { "c", "lua", "vim" },
+
+          sync_install = false,
+
+          auto_install = true,
+
+
+          highlight = {
+              enable = true,
+              additional_vim_regex_highlighting = false,
+          },
+      }
+
+  end
 }
