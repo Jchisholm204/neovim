a350d650f35ae76667193329484d7383cccab087
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-09-11 00:49:08 -0400

    remap jump definition

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover)
+-- Jump to definition
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 
 -- use spaces instead of tabs
 vim.o.expandtab = true
