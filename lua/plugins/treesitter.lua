7fb017f5d5f2049a20d0c8c81a57ff05cb020779
Jacob Chisholm <jacobchisholm1010@gmail.com>
2024-09-11 01:07:56 -0400

    ts fixed?

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,14 @@
+-- treesitter.lua
 return {
-  "nvim-treesitter/nvim-treesitter",
-  version = false, -- last release is way too old and doesn't work on Windows
-  build = ":TSUpdate",
-  lazy = false, 
-  priority = 1000,
-  cmd = {"TSUpdate", "TSInstall"},
-  config = function()
-      require'nvim-treesitter.configs'.setup {
-          -- A list of parser names, or "all" (the five listed parsers should always be installed)
-          ensure_installed = { "c", "lua", "vim" },
-
-          sync_install = false,
-
-          auto_install = true,
-
-
-          highlight = {
-              enable = true,
-              additional_vim_regex_highlighting = false,
-          },
-      }
-
-  end
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate', -- Automatically update parsers on install
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = { "cpp", "lua", "python", "c" }, -- Minimal set of languages
+            highlight = {
+                enable = true, -- Enable syntax highlighting
+            },
+        }
+    end,
 }
+
