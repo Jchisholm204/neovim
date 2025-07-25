674930889331b70208c36ef261f2fde3303bc0e2
Jacob Chisholm <jacobchisholm1010@gmail.com>
2025-07-25 09:30:34 -0700

    idk systemv

--- init.lua
+++ init.lua
@@ -3,4 +3,6 @@ require("config.lazy")
 -- require 'nvim-treesitter.install'.compilers = {"clang","gcc"}
 -- Enable Line Numbers
 vim.o.number = true
+vim.g.python3_host_prog = '/bin/python3.10'
+
 
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -15,47 +15,87 @@ return {
                 icons = {
                     package_installed = "✓",
                     package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
+                    package_uninstalled = "✗",
+                },
+            },
+        })
+
+        -- mason_lspconfig.setup({
+        --     ensure_installed = {
+        --         'lua_ls',
+        --         -- C/C++ Language Servers
+        --         'clangd',
+        --         -- 'cmake',
+        --         -- 'arduino_language_server',
+        --         -- Rust
+        --         -- 'rust_analyzer',
+        --         -- Bash
+        --         'bashls',
+        --         -- Assembly Language LSP
+        --         -- 'asm_lsp',
+        --         -- Latex LSP
+        --         'ltex',
+        --         -- 'bufls',
+        --         -- VHDL/Verilog/SystemVerilog LSP
+        --         -- 'hdl_checker',
+        --         -- Python LSP
+        --         -- 'pylsp',
+        --         -- 'gh_actions',
+        --         'svls',
+        --     },
+        --     -- You can disable automatic installation if you want:
+        --     -- automatic_installation = true,
+        -- })
+
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+        -- Setup servers individually
+        local servers = {
+            'lua_ls',
+            'clangd',
+            'bashls',
+            'ltex',
+            'pylsp',
+            'cmake',
+            -- Add more servers here if you uncomment them above
+        }
+
+        for _, server in ipairs(servers) do
+            lspconfig[server].setup({
+                capabilities = capabilities,
+            })
+        end
+
+        -- Custom setup for svls
+        lspconfig.svls.setup({
+            capabilities = capabilities,
+            filetypes = { "verilog", "systemverilog" },
+            cmd = {"svls"},
+            root_dir = lspconfig.util.root_pattern(".git", "hdl", ".", ".svproject"),
+        })
+
+        lspconfig.svlangserver.setup({
+            cmd = { "svlangserver", "--stdio" },
+            filetypes = { "verilog", "systemverilog" },
+            root_dir = lspconfig.util.root_pattern(".git", "."),
+            capabilities = capabilities,
+            init_options = {
+                includeIndexing = {"**/*.sv", "**/*.v", "**/*.svh"}
             }
         })
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                'lua_ls',
-                -- C/C++ Languager Servers
-                'clangd',
-                'cmake',
-                'arduino_language_server',
-                -- Rust
-                'rust_analyzer',
-                -- Bash
-                'bashls',
-                -- Assembly Language LSP
-                'asm_lsp',
-                -- Latex LSP
-                'ltex',
-                -- 'bufls',
-                -- VHDL/Verilog/SystemV LSP
-                'hdl_checker',
-                -- Python LSP
-                'pylsp',
-            },
-            -- for the love of god I dont want to worry about this
-            automatic_installation = true,
+        lspconfig.verible.setup({
+            cmd = { "verible-verilog-ls", "--rules_config_search" },
+            filetypes = { "verilog", "systemverilog" },
+            root_dir = lspconfig.util.root_pattern(".git", "verilator.f"),
+            -- format_on_save = true,
         })
 
-        local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = lsp_capabilities,
-                })
-            end,
-        })
+        -- require'plugins.lsp.setup_systemverilog'.setupLsp()
+
         vim.diagnostic.config({
             update_in_insert = true,
         })
-
     end,
 }
+
