fd0fb441c63301d74b9bf1a10f4a3169f360dd18
Jacob Chisholm <jacobchisholm1010@gmail.com>
2026-05-15 14:48:20 -0700

    new version of lspconfig fixes

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -8,7 +8,6 @@ return {
     config = function()
         local mason = require('mason')
         local mason_lspconfig = require('mason-lspconfig')
-        local lspconfig = require('lspconfig')
 
         mason.setup({
             ui = {
@@ -20,74 +19,61 @@ return {
             },
         })
 
-        -- mason_lspconfig.setup({
-        --     ensure_installed = {
-        --         'lua_ls',
-        --         -- C/C++ Language Servers
-        --         'clangd',
-        --         -- 'cmake',
-        --         -- 'arduino_language_server',
-        --         -- Rust
-        --         -- 'rust_analyzer',
-        --         -- Bash
-        --         'bashls',
-        --         -- Assembly Language LSP
-        --         -- 'asm_lsp',
-        --         -- Latex LSP
-        --         'ltex',
-        --         -- 'bufls',
-        --         -- VHDL/Verilog/SystemVerilog LSP
-        --         -- 'hdl_checker',
-        --         -- Python LSP
-        --         -- 'pylsp',
-        --         -- 'gh_actions',
-        --         'svls',
-        --     },
-        --     -- You can disable automatic installation if you want:
-        --     -- automatic_installation = true,
-        -- })
+        mason_lspconfig.setup({
+            ensure_installed = {
+                'lua_ls',
+                -- C/C++ Language Servers
+                'clangd',
+                -- 'cmake',
+                -- 'arduino_language_server',
+                -- Rust
+                -- 'rust_analyzer',
+                -- Bash
+                'bashls',
+                -- Assembly Language LSP
+                -- 'asm_lsp',
+                -- Latex LSP
+                'ltex',
+                -- 'bufls',
+                -- VHDL/Verilog/SystemVerilog LSP
+                -- 'hdl_checker',
+                -- Python LSP
+                -- 'pylsp',
+                -- 'gh_actions',
+                'svls',
+            },
+            -- You can disable automatic installation if you want:
+            -- automatic_installation = true,
+        })
 
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-        -- Setup servers individually
-        local servers = {
-            'lua_ls',
-            'clangd',
-            'bashls',
-            'ltex',
-            'pylsp',
-            'cmake',
-            -- Add more servers here if you uncomment them above
-        }
-
-        for _, server in ipairs(servers) do
-            lspconfig[server].setup({
-                capabilities = capabilities,
-            })
-        end
+        vim.lsp.config('*', {
+            capabilities = capabilities,
+        })
 
         -- Custom setup for svls
-        lspconfig.svls.setup({
+        vim.lsp.config('svls', {
             capabilities = capabilities,
             filetypes = { "verilog", "systemverilog" },
             cmd = {"svls"},
-            root_dir = lspconfig.util.root_pattern(".git", "hdl", ".", ".svproject"),
+            root_markers = {".git", "hdl", ".", ".svproject"},
         })
 
-        lspconfig.svlangserver.setup({
+        vim.lsp.config('svlangserver', {
             cmd = { "svlangserver", "--stdio" },
             filetypes = { "verilog", "systemverilog" },
-            root_dir = lspconfig.util.root_pattern(".git", "."),
+            root_markers = {".git", "hdl", ".", ".svproject"},
             capabilities = capabilities,
             init_options = {
                 includeIndexing = {"**/*.sv", "**/*.v", "**/*.svh"}
             }
         })
 
-        lspconfig.verible.setup({
+        vim.lsp.config('verible', {
             cmd = { "verible-verilog-ls", "--rules_config_search" },
             filetypes = { "verilog", "systemverilog" },
-            root_dir = lspconfig.util.root_pattern(".git", "verilator.f"),
+            root_markers = {".git", "hdl", ".", "verilator.f"},
             -- format_on_save = true,
         })
 
